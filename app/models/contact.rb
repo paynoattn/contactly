dce75192b42f1884cdf5ccfd7d81591db6be88d5
Chris Pawlukiewicz <chris.pawlukiewicz@thomasarts.com>
2015-06-03 21:55:41 -0600

    Let's setup some routing

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,9 @@ class Contact
   field :email, type: String
   field :phone, type: Integer
   field :address, type: String
+  field :_id, type: String, default: ->{ name.to_s.parameterize }
+
+  def validation
+    validates_presence_of :name, :email, :phone, :address
+  end
 end
